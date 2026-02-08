f6b016d6af27abe6a58eab3cbdbb055baa35575b
Nacho <nacho@asd.com>
2026-02-08 20:14:27 +0100

    se agrega clase de configuración de proveedores y modelos de IA

--- ai-orchestrator/src/main/java/my/aiorchestrator/AiOrchestratorApplication.java
+++ ai-orchestrator/src/main/java/my/aiorchestrator/AiOrchestratorApplication.java
@@ -1,12 +1,15 @@
 package my.aiorchestrator;
 
+import my.aiorchestrator.configuration.AiModelsConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AiModelsConfig.class)
 public class AiOrchestratorApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(AiOrchestratorApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(AiOrchestratorApplication.class, args);
+    }
 }
---
+++ ai-orchestrator/src/main/java/my/aiorchestrator/configuration/AiModelsConfig.java
@@ -0,0 +1,33 @@
+package my.aiorchestrator.configuration;
+
+import java.util.Map;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "ai")
+public record AiModelsConfig(Map<String, AiProvider> providers) {
+
+    public record AiProvider(String baseUrl, String apiKey, Map<String, AiModel> models) {
+
+        public record AiModel(float temperature, int maxTokens) {}
+    }
+
+    public record AiConfig(String baseUrl, String apiKEy, float temperature, int maxTokens) {}
+
+    public AiConfig getConfig(String provider, String model) {
+
+        AiProvider aiProvider = providers.get(provider);
+
+        if (aiProvider == null) {
+            throw new IllegalArgumentException("Unknown provider: " + provider);
+        }
+
+        AiProvider.AiModel aiModel = aiProvider.models.get(model);
+
+        if (aiModel == null) {
+            throw new IllegalArgumentException("Unknown model: " + model);
+        }
+
+        return new AiConfig(
+                aiProvider.baseUrl, aiProvider.apiKey, aiModel.temperature, aiModel.maxTokens);
+    }
+}
