686df272d572c5db1c27efda12a73e4e9f170134
Nacho <nacho@asd.com>
2026-02-15 21:28:38 +0100

    servicio con las operaciones básicas de S3

---
+++ ai-orchestrator/src/main/java/my/aiorchestrator/application/ports/outgoing/S3Service.java
@@ -0,0 +1,16 @@
+package my.aiorchestrator.application.ports.outgoing;
+
+import io.awspring.cloud.s3.S3Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+public interface S3Service {
+
+    void pushMultipartFileToBucket(MultipartFile file, String bucket) throws IOException;
+
+    S3Resource downloadFromBucket(String bucket, String key);
+
+    void deleteFileFromBucket(String bucket, String key);
+}
---
+++ ai-orchestrator/src/main/java/my/aiorchestrator/infrastructure/adapters/S3ServiceAdapter.java
@@ -0,0 +1,40 @@
+package my.aiorchestrator.infrastructure.adapters;
+
+import io.awspring.cloud.s3.S3Resource;
+import io.awspring.cloud.s3.S3Template;
+import lombok.RequiredArgsConstructor;
+import my.aiorchestrator.application.ports.outgoing.S3Service;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+import java.io.*;
+import java.net.URI;
+
+@Service
+@RequiredArgsConstructor
+public class S3ServiceAdapter implements S3Service {
+
+    private final S3Template s3Template;
+
+    public void pushMultipartFileToBucket(MultipartFile file, String bucket) throws IOException {
+
+        s3Template.upload(bucket, file.getName(), file.getInputStream());
+    }
+
+    @Override
+    public S3Resource downloadFromBucket(String bucket, String key) {
+
+        return s3Template.download(bucket, key);
+    }
+
+    @Override
+    public void deleteFileFromBucket(String bucket, String key) {
+
+        s3Template.deleteObject(bucket, key);
+    }
+}
